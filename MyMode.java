03f401da315cd1d86af12cbdc798accd332af18c
1usama <49752485+1usama@users.noreply.github.com>
2019-10-08 19:45:09 +0500

    Add files via upload

---
+++ MyMode.java
@@ -0,0 +1,54 @@
+package spec;
+import java.util.Random;
+public class MyMode implements mode {
+
+	
+	static int N = 20;
+    static int[] sequence = new int[N];
+	@Override
+	public int getmode(int a[]) {
+		
+	    
+	    int maxValue = 0, maxCount = 0;
+	    
+        for (int i = 0; i < sequence.length; ++i) 
+        {
+            int count = 0;
+            for (int j = 0; j < sequence.length; ++j) 
+            {
+                if (sequence[j] == sequence[i])
+                    ++count;
+            }
+            if (count > maxCount) 
+            {
+                maxCount = count;
+                maxValue = sequence[i];
+            }
+        }
+ 
+        return maxValue;
+		
+		
+	      
+	   }
+	   public static void main(String args[]){
+
+		   Random random = new Random();
+		   
+	        for (int i = 0; i < N; i++)
+	            sequence[i] = Math.abs(random.nextInt(100));
+	 
+	        System.out.println("The set of numbers are: ");
+	        for (int i = 0; i < N; i++)
+	            System.out.print(sequence[i] + " ");
+	 
+	        System.out.println("\nThe mode of the set is: " + getmode());
+	   }
+	   
+		
+		
+	}
+
+	
+
+
---
+++ mode.java
@@ -0,0 +1,10 @@
+package spec;
+
+public interface mode {
+	/**
+	 * Find one of repeated integer in array
+	 * @param an array which contains more than one value and repeation of values
+	 * @return a number that is repeated  
+	 */
+	public int getmode(int[ ] val);
+}
